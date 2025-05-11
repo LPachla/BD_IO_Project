63c6c8717d9237ed5ee938eb456ed0487720519e
Damian Pietryka <damianpietryka2003@gmail.com>
2025-05-11 20:34:41 +0200

    Split schema file into more specific files

---
+++ db/01_database_schema.sql
@@ -0,0 +1,20 @@
+CREATE TABLE powiaty (
+    id SERIAL PRIMARY KEY,
+    powiat VARCHAR(255)
+);
+
+CREATE TABLE atrakcje(
+    id SERIAL PRIMARY KEY,
+    nazwa VARCHAR(255),
+    powiat INT references powiaty(id),
+    opis TEXT,
+    lokalizacjaX VARCHAR(30),
+    lokalizacjaY VARCHAR(30),
+    ocena REAL
+);
+
+CREATE TABLE zdjecia(
+    id SERIAL PRIMARY KEY,
+    atrakcja INT references atrakcje(id),
+    zdjecia TEXT
+);
