2a90265104b458494ae2c7d899a5d807ff193734
Mon011 <61294568+Mon011@users.noreply.github.com>
2025-05-18 19:06:50 +0200

    FRONT: Navigation (#6)
    * init navigation
    
    * add navigation & merge develop
    
    ---------
    
    Co-authored-by: Jakub Porzuczek <jakub.porzuczek@poccofinance.com>

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,9 +1,23 @@
+import Favorite from "./pages/Favorite";
+import Home from "./pages/Home";
+import Details from "./pages/Details";
 import "./styles.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { MantineProvider} from '@mantine/core'
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
-    </div>
+    <MantineProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route index element = {<Home/>}/>
+          <Route path = "details" element = {<Details/>}/>
+          <Route path = "favorite" element = {<Favorite/>}/>
+        </Route>
+      </Routes>    
+    </BrowserRouter>
+    </MantineProvider>
   );
+  
 }
