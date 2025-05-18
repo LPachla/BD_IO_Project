e7759a5cd7e0e750bc4348f45c92c555f84528da
Jakub Porzuczek <jakub.porzuczek@poccofinance.com>
2025-05-18 14:42:53 +0200

    add navigation & merge develop

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -3,17 +3,21 @@ import Home from "./pages/Home";
 import Details from "./pages/Details";
 import "./styles.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { MantineProvider} from '@mantine/core'
 
 export default function App() {
   return (
+    <MantineProvider>
     <BrowserRouter>
       <Routes>
         <Route path="/">
           <Route index element = {<Home/>}/>
           <Route path = "details" element = {<Details/>}/>
-          <Route index = "favorite" element = {<Favorite/>}/>
+          <Route path = "favorite" element = {<Favorite/>}/>
         </Route>
       </Routes>    
     </BrowserRouter>
+    </MantineProvider>
   );
+  
 }
