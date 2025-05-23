8ac14f39e0e7f155a142a37c81023ebbe1bc40dd
Jakub Porzuczek <porzuczek.jakub@outlook.com>
2025-05-23 18:07:17 +0200

    WIP

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,7 +1,14 @@
+import '@mantine/core/styles.css';
+import { createTheme, MantineProvider} from '@mantine/core';
+
 import MapPage from './pages/MapPage';
 
 function App() {
-  return <MapPage />;
+  return (
+    <MantineProvider>
+    <MapPage />
+    </MantineProvider>
+  );
 }
 
 export default App;
