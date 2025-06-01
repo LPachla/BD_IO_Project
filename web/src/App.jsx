2ced98ba10e9ab78f0bdd6bbf49c38493f7bd361
Milias <159380701+Mil1as@users.noreply.github.com>
2025-06-01 16:05:17 +0200

    Mainpage (#10)
    * Example mainpage
    
    * fix vite build
    
    * Theming (#8)
    
    * WIP
    
    * Add theming, navigation and dummy pages
    
    ---------
    
    Co-authored-by: Jakub Porzuczek <porzuk@KE-O-PC071.local>
    
    * Added integration with backend
    
    * Removed attractions.json
    
    ---------
    
    Co-authored-by: Jakub Porzuczek <porzuczek.jakub@outlook.com>
    Co-authored-by: Mon011 <61294568+Mon011@users.noreply.github.com>
    Co-authored-by: Jakub Porzuczek <porzuk@KE-O-PC071.local>
    Co-authored-by: Damian Pietryka <damianpietryka2003@gmail.com>

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,23 +1,44 @@
-import Favorite from "./pages/Favorite";
-import Home from "./pages/Home";
-import Details from "./pages/Details";
-import "./styles.css";
+import '@mantine/core/styles.css';
+import { createTheme, MantineProvider} from '@mantine/core';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { MantineProvider} from '@mantine/core'
 
-export default function App() {
+import MapPage from './pages/MapPage';
+import PopularPage from './pages/PopularPage';
+import DetailsPage from './pages/DetailsPage';
+
+const theme = createTheme({
+  green: [
+    '#f1faed',
+    '#d6e8ce',
+    '#c8dfbd',
+    '#a9cd99',
+    '#8fbe7a',
+    '#7eb466',
+    '#75b05b',
+    '#639a4b',
+    '#578940',
+    '#487733'
+  ],
+  headings: {
+    fontFamily: 'Georgia, serif',
+    fontWeight: 100
+  }
+});
+
+function App() {
   return (
-    <MantineProvider>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/">
-          <Route index element = {<Home/>}/>
-          <Route path = "details" element = {<Details/>}/>
-          <Route path = "favorite" element = {<Favorite/>}/>
-        </Route>
-      </Routes>    
-    </BrowserRouter>
+    <MantineProvider theme={theme}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route index element = {<MapPage/>}/>
+            <Route path = "details" element = {<DetailsPage/>}/>
+            <Route path = "popular" element = {<PopularPage/>}/>
+          </Route>
+        </Routes>    
+      </BrowserRouter>
     </MantineProvider>
   );
-  
 }
+
+export default App;
